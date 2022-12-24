e1935e1d2bedddd248da7e7f7a997b81d1e890d6
AlexeyRogS <pizikxxx97@mail.ru>
2022-12-24 14:02:32 +0300

    added lesson8

---
+++ lesson8/App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Homework;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var arr = new int[,,] {{{66, 27}, {25, 90}},
+                                   {{34, 26}, {41, 55}}};
+            Class1.Task60(arr);
+        }
+    }
+}
